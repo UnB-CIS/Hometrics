1c7efc829b7a495e8dc1b0c5e8dc21b17229229f
LuizPaulo23 <luizpauloueg@gmail.com>
2024-09-30 10:49:09 -0300

    CHANGED: ideias iniciais para o filtro de anomalia

--- scripts/functions/get_censorship.R
+++ scripts/functions/get_censorship.R
@@ -1,10 +1,19 @@
+#' @title Função de filtro de censura 
+#' @details Forma de filtrar as anomalias 
+#' @author Luiz Paulo Tavares 
+
+
 get_censorship <- function(){
+  
+  # ideia geral: partindo da métrica de média aparada 
+  # censurar % do início da distribuição e do final da distribuição 
+  # ou seja, censurar as caldas da distribuição 
   
   n <- nrow(data_raw) * 0.05
   
   ordenado <- data_raw %>% 
-    dplyr::arrange(price) %>% 
-    dplyr::slice(1601:(n() - 1600))
+              dplyr::arrange(price) %>% 
+              dplyr::slice(1601:(n() - 1600))
   
   
 }
