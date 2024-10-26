eafaf9b6d85a209de142bc48f2cd6813340ab56c
Caetano <caefleury15@gmail.com>
2024-10-26 15:17:31 -0300

    feat(db): Cria instancia do mongodb e insere dados de teste

---
+++ db/mongodb.py
@@ -0,0 +1,59 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+def connect_to_mongo(uri="mongodb://localhost:27017/"):
+    client = MongoClient(uri)
+    return client
+
+def insert_property(client, data):
+    db = client.housingprices
+    property_listings = db.property_listings
+    return property_listings.insert_one(data).inserted_id
+
+def insert_multiple_properties(client, properties_list):
+    db = client.housingprices
+    property_listings = db.property_listings
+    # Adicionar timestamp atual a cada propriedade
+    for property_data in properties_list:
+        property_data['timestamp'] = datetime.now()
+    result = property_listings.insert_many(properties_list)
+    return result.inserted_ids
+
+client = connect_to_mongo()
+
+test_data = [
+    {
+        "state": "DF",
+        "city": "Brasília",
+        "description": "SHS Quadra 06 Conjunto A Bloco D, ASA SUL, BRASILIA",
+        "type": "Aluguel Loja 86 m²",
+        "price": 10.285,
+        "size": 86,
+        "bedrooms": None,
+        "cars_spaces": None,
+    },
+    {
+        "state": "SP",
+        "city": "São Paulo",
+        "description": "Av. Paulista, Bela Vista",
+        "type": "Venda Apartamento 120 m²",
+        "price": 1.250,
+        "size": 120,
+        "bedrooms": 3,
+        "cars_spaces": 2,
+    },
+    {
+        "state": "RJ",
+        "city": "Rio de Janeiro",
+        "description": "Rua Barata Ribeiro, Copacabana",
+        "type": "Venda Apartamento 80 m²",
+        "price": 900,
+        "size": 80,
+        "bedrooms": 2,
+        "cars_spaces": 1,
+    }
+]
+
+inserted_ids = insert_multiple_properties(client, test_data)
+
+print(f"Property inserted with ID: {inserted_ids}")
